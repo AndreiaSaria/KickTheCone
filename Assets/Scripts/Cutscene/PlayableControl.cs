bad06bb0b7d70ae460a8def6f1acc3fbf59cd738
Andréia/Saria <63603210+AndreiaSaria@users.noreply.github.com>
2020-04-26 18:24:55 -0300

    Timeline Updated but still Malfunctioning

---
+++ Assets/Scripts/Cutscene/PlayableControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.Playables;
+using UnityEngine;
+
+public class PlayableControl : MonoBehaviour //Isso serve para resetar a timeline caso o player a queira rever
+{
+    //https://forum.unity.com/threads/reset-timeline-from-code.503904/ não funciona
+
+    public PlayableDirector director;
+
+    private void Start()
+    {
+        director.initialTime = 0;
+        director.time = 0;
+        director.Stop();
+        director.Evaluate();
+        director.Play();
+    }
+
+}
---
+++ Assets/Scripts/Cutscene/UnityChanControlScene.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Script com partes do UnityChanForAndroidDodge
+//Apenas para a cutscene
+public class UnityChanControlScene : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody rb;
+    private float speedYBefore;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+
+        speedYBefore = transform.position.y;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        anim.SetFloat("Speed", rb.velocity.magnitude); //Enviando Inputs de movimento para o animator
+        anim.speed = 1.2f; //Velocidade que roda as animações no animator
+
+        if (Mathf.Abs(rb.worldCenterOfMass.y) > 1f) 
+        {
+            anim.SetBool("SpeedY", true);
+            speedYBefore = transform.position.y;
+        }
+        else { anim.SetBool("SpeedY", false); }
+
+
+        rb.AddForce(transform.forward * 2 * (1 + Time.fixedDeltaTime) * 8, ForceMode.Impulse); //velocidade mais realística
+    }
+}
